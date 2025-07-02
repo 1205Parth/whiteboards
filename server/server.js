35d11e27e0f6f80425228839cb6957166f3b7921
Parth Vaghasiya <162993263+1205Parth@users.noreply.github.com>
2025-07-02 18:03:08 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -15,10 +15,13 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect("mongodb+srv://parth:1205@parth.ag7kdby.mongodb.net/?retryWrites=true&w=majority&appName=parth", {
+mongoose
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}).then(() => console.log("✅ MongoDB connected"));
+  })
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch((err) => console.error("❌ MongoDB connection error:", err));
 
 // REST API to join/create room
 app.post('/api/rooms/join', async (req, res) => {
